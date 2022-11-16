a7d625d89a8a34a3907956498d76cb42fcd00e7b
Areeb <auhmirza02@gmail.com>
2022-11-16 22:13:03 +0000

    3.4: Implement functionality to delete single entry

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -3,51 +3,60 @@ const express = require("express")
 const app = express()
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
 let info = `Phonebook has info for ${persons.length} people`
 let date = new Date()
-            
 
-app.get('/api/persons',(request,response)=>{
-    
+
+app.get('/api/persons', (request, response) => {
+
     response.json(persons)
 })
 
-app.get('/api/persons/:id',(request,response)=>{
+app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
-    
-    response.json(person)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
-app.get('/info',(request,response)=>{
+app.get('/info', (request, response) => {
     let info = `Phonebook has info for ${persons.length} people`
     let date = new Date().toString()
-            
-    response.end(`${info}\n${date}`)   
+
+    response.end(`${info}\n${date}`)
+})
+
+app.delete("/api/persons/:id",(request,response)=>{
+    const id = Number(request.params.id)
+    persons= persons.filter(person => person.id !== id)
 })
 
 const PORT = 3001
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })

0234e6968ab62a10db182b633bb7438fa4b617a4
Areeb <auhmirza02@gmail.com>
2022-11-27 17:52:48 +0000

    3.16 Add error handling middleware

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -45,7 +45,6 @@ app.delete("/api/persons/:id", (request, response) => {
 })
 
 
-
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
@@ -69,6 +68,23 @@ app.post('/api/persons', (request, response) => {
         })
 
     })
+
+    const unknownEndpoint = (request, response) => {
+        response.status(404).send({ error: 'unknown endpoint' })
+      }
+      
+      app.use(unknownEndpoint)
+ 
+const errorHandler = (error ,request, response, next) => {
+    console.error(error.message)
+   
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+      }
+      next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
